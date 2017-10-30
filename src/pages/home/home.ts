25edfbb817840699b05401d7c761048ca8a763fa
monashtharuka <monashperera2@gmail.com>
2017-10-30 15:40:28 +0530

    post

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
+import{ Http,Headers} from'@angular/http';
 import { NavController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  data : any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http) {
 
+  }
+  ionview(){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let body = {
+      security:"LL9Ny4IF",
+      email: "you@yourmail.com",
+      password: 123124
+
+
+
+    };
+    this.http.post('https://api.pathola.com/freelance_login_submit.php?',body,{headers: headers})
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log(data);
+    })
   }
 
 }
